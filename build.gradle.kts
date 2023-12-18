50503d97da4953cd1f4cb3ee86a2f4a31bccafb3
Ryan Dens <ryan.dens@pixee.ai>
2023-12-18 09:39:55 -0800

    :tada:

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    java
+}
+
+repositories {
+    mavenCentral()
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
+
---
+++ src/main/java/com/ryadens/example/ScriptRunner.java
@@ -0,0 +1,30 @@
+package com.ryadens.example;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.attribute.PosixFilePermission;
+import java.nio.file.attribute.PosixFilePermissions;
+import java.util.Set;
+import java.util.HashSet;
+import java.nio.file.Files;
+
+public class ScriptRunner {
+
+    public void create(Path script) throws IOException {
+        Set<PosixFilePermission> filePermissions = new HashSet<PosixFilePermission>();
+        filePermissions.add(PosixFilePermission.OTHERS_WRITE);
+        filePermissions.add(PosixFilePermission.OTHERS_EXECUTE);
+        Files.setPosixFilePermissions(script,filePermissions);
+        Runtime.getRuntime().exec(script.toString());
+    }
+
+    public void updateNewScript(Path startupScript, Path shutdownScript) throws IOException {
+        // why not have anybody be able to execute?
+        Files.setPosixFilePermissions(startupScript, PosixFilePermissions.fromString("rwxrwx---"));
+
+        // same for script
+        Set<PosixFilePermission> permissions = PosixFilePermissions.fromString("rwxrwx---");
+        Files.setPosixFilePermissions(shutdownScript, permissions);
+    }
+
+}
